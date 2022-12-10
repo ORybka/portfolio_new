0d72d2e82b912d86105669d9acf2d99b06dc4629
ORybka <olha.rybchynska@gmail.com>
2022-12-10 13:35:27 +0200

    add basic code and files

---
+++ src/pages/about/EducationPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EducationPage.scss';
+
+function EducationPage() {
+    return (
+        <div className='education-page-container'>
+            <h1 className='main-title'>Education Page</h1>
+        </div>
+    );
+}
+
+export default EducationPage;
---
+++ src/pages/about/PreviousPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PreviousPage.scss';
+
+function PreviousPage() {
+    return (
+        <div className='previous-page-container'>
+            <h1 className='main-title'>Previous Page</h1>
+        </div>
+    );
+}
+
+export default PreviousPage;

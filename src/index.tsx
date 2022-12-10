86e116f10a4ce32691e5be13e6f1e8fdd8f56dd7
ORybka <olha.rybchynska@gmail.com>
2022-12-10 13:50:22 +0200

    add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import App from './components/App';
 import './reset.scss';
 import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
     <App />
